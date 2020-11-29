84316a9e69d599f75f55ce6921f92365b3553b7c
Adel Abdelhak <6298183+blakelead@users.noreply.github.com>
2020-11-29 23:18:15 +0100

    add: output variables (#1)

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,16 @@ output "id" {
   value       = azurerm_automation_account.auto_account.id
 }
 
+output "name" {
+  description = "The Automation Account name."
+  value       = var.name
+}
+
+output "resource_group_name" {
+  description = "The Automation Account resource group name."
+  value       = var.resource_group_name
+}
+
 output "endpoint" {
   description = "The DSC Server Endpoint associated with this Automation Account."
   value       = azurerm_automation_account.auto_account.dsc_server_endpoint
